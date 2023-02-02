b27fc2220a815b6c037617f0a486fbc192c1fd47
brennanp93 <brennan.p.perez@gmail.com>
2023-02-01 19:40:04 -0800

    added a hint component to clean up main page

---
+++ src/components/HintPage/HintPage.jsx
@@ -0,0 +1,31 @@
+export default function HintPage({ currentMovie, numGuesses }) {
+  return (
+    <>
+      {
+        numGuesses === 2 ?
+          <h3> Hint 1: {currentMovie?.hints[0]}</h3>
+          :
+          ''
+      }
+      {
+        numGuesses === 1 ?
+          <>
+            <h3> Hint 1: {currentMovie?.hints[0]}</h3>
+            <h3> Hint 2: {currentMovie?.hints[1]}</h3>
+          </>
+          :
+          ''
+      }
+      {
+        numGuesses === 0 ?
+          <>
+            <h3> Hint 1: {currentMovie?.hints[0]}</h3>
+            <h3> Hint 2: {currentMovie?.hints[1]}</h3>
+            <h3> Hint 3: {currentMovie?.hints[2]}</h3>
+          </>
+          :
+          ''
+      }
+    </>
+  )
+}
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import SubmitForm from "../../components/SubmitForm/SubmitForm"
 import ResultPage from "../../components/ResultPage/ResultPage";
+import HintPage from "../../components/HintPage/HintPage";
 import Fuse from "fuse.js";
 import { quizlist } from "../../data";
 // import { quizlist } from "../../data";
@@ -9,32 +10,20 @@ import "./MainPage.css"
 
 
 export default function MainPage({ score, setScore, dailyQuestion, answerKey, cookies, setCookies, updateCount }) {
-  let currentMovie = dailyQuestion
   const [prompt, setPrompt] = useState('');
   const [incomingGuess, setIncomingGuess] = useState('')
-  // const [index, setIndex] = useState(Math.floor(Math.random() * quizlist.length))
-  const [hintOne, setHintOne] = useState('')
-  const [hintTwo, setHintTwo] = useState('')
-  const [hintThree, setHintThree] = useState('')
-  // const [numGuesses, setNumGuesses] = useState(3);
   const [numGuesses, setNumGuesses] = useState(() => {
     let savedGuesses = localStorage.getItem('numGuesses');
-    return (parseInt(savedGuesses) || 3)
+    return (parseInt(savedGuesses) ?? 3)
   });
-  // const [cookies, setCookies, removeCookies] = useCookies(['date'])
-  // const [winner, setWinner] = useState(false);
-  // const [buttonPrompt, setButtonPrompt] = useState('Next Question');
-  // const [count, setCount] = useState(0);
-  // const [dailyQuestion1, setDailyQuestion] = useState(dailyQuestion)
-
-  // let currentMovie = quizlist[index]
-  // console.log(currentMovie)
 
   useEffect(() => {
     localStorage.setItem('numGuesses', numGuesses);
     // localStorage.setItem('hintOne', hintOne);
   }, [numGuesses])
 
+  let currentMovie = dailyQuestion
+  console.log(numGuesses)
 
   let correctAnswer = currentMovie?.movie;
 
@@ -63,8 +52,7 @@ export default function MainPage({ score, setScore, dailyQuestion, answerKey, co
       setIncomingGuess('');
       setNumGuesses(3)
       setCookies('date', todayDate, { expires: midnight })
-      currentMovie.count = currentMovie.count + 1
-
+      currentMovie.count += 1;
       if (numGuesses === 3) {
         setScore(score + 4)
       } else if (numGuesses === 2) {
@@ -73,22 +61,19 @@ export default function MainPage({ score, setScore, dailyQuestion, answerKey, co
         setScore(score + 2)
       } else if (numGuesses === 0) {
         setScore(score + 1)
-      } else if (numGuesses === -1) {
-        setScore(score + 0)
-      };
-
+      }
     } else if (result !== correctAnswer && numGuesses === 3) {
       setNumGuesses(numGuesses - 1);
       // setCookies('numGuesses', numGuesses, { expires: midnight })
-      setHintOne('Hint One: ' + currentMovie.hints[0])
+      // setHintOne('Hint One: ' + currentMovie.hints[0])
       setIncomingGuess('');
     } else if (result !== correctAnswer && numGuesses === 2) {
       setNumGuesses(numGuesses - 1);
-      setHintTwo('Hint Two: ' + currentMovie.hints[1])
+      // setHintTwo('Hint Two: ' + currentMovie.hints[1])
       setIncomingGuess('');
     } else if (result !== correctAnswer && numGuesses === 1) {
       setNumGuesses(numGuesses - 1);
-      setHintThree('Hint Three: ' + currentMovie.hints[2])
+      // setHintThree('Hint Three: ' + currentMovie.hints[2])
       setIncomingGuess('');
     }
     if (result !== correctAnswer && numGuesses === 0) {
@@ -114,34 +99,9 @@ export default function MainPage({ score, setScore, dailyQuestion, answerKey, co
           <h1>Guesses Remaining: {numGuesses}</h1>
           <div>
             <img className="image" src={currentMovie?.image} alt="" />
+
           </div>
-          {
-            numGuesses === 2 ?
-              <h3> Hint 1: {currentMovie.hints[0]}</h3>
-              :
-              ''
-          }
-          {
-            numGuesses === 1 ?
-              <>
-                <h3> Hint 1: {currentMovie.hints[0]}</h3>
-                <h3> Hint 2: {currentMovie.hints[1]}</h3>
-              </>
-              :
-              ''
-          }
-          {
-            numGuesses === 0 ?
-              <>
-                <h3> Hint 1: {currentMovie.hints[0]}</h3>
-                <h3> Hint 2: {currentMovie.hints[1]}</h3>
-                <h3> Hint 3: {currentMovie.hints[2]}</h3>
-              </>
-              :
-              ''
-          }
-          {/* <h3>{hintTwo}</h3>
-          <h3>{hintThree}</h3> */}
+          <HintPage numGuesses={numGuesses} currentMovie={currentMovie} />
           <SubmitForm handleSubmit={handleSubmit}
             incomingGuess={incomingGuess}
             handleChange={handleChange}
