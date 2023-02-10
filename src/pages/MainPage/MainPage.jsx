d019d8f261db7363f981467f9f667db8c96f0d15
brennanp93 <brennan.p.perez@gmail.com>
2023-02-09 20:56:28 -0800

    seed db

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -94,6 +94,7 @@ export default function MainPage({ score, setScore, dailyQuestion, answerKey, co
           <h1>Guesses Remaining:  {numGuesses}</h1>
           <div>
             <img className="image" src={currentMovie?.image} alt="" />
+            {/* <img className="image" src='v' alt="" /> */}
 
           </div>
           <HintPage
