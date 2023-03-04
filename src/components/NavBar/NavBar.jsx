4906f89ac114aba3a14d227fdedf6478dc3732af
brennanp93 <brennan.p.perez@gmail.com>
2023-03-04 12:25:17 -0800

    minor css

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,9 +7,9 @@ export default function NavBar({ score, setAboutPage, aboutPage }) {
     <header>
       <button className="link-button" onClick={() => setAboutPage(false)}>How Do I Play?</button>
       <h1 className='app-title'>Name That Movie!</h1>
-      <div>
+      {/* <div> */}
         <h3 className="game-score">Current Score: {score}</h3>
-      </div>
+      {/* </div> */}
     </header>
   );
 }
