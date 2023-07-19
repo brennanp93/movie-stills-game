8bdd143724d13b5a9d1b960342f2edceb8042006
brennanp93 <brennan.p.perez@gmail.com>
2023-07-19 10:29:55 -0700

    update verbiage on about page.

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -7,8 +7,8 @@ export default function AboutPage({ setAboutPage }) {
         <ul>
           <li>A new frame from a popular movie will be displayed every day.</li>
           <li>The player has 4 chances to guess the name of the movie.</li>
-          <li>After each incorrect guess, one of three hints is displayed.</li>
-          {/* <li>There are a total of three hints.</li> */}
+          <li>After each incorrect guess, a new hint is displayed.</li>
+          <li>There are a total of three hints.</li>
         </ul>
         <div>
           <button className="play-button" onClick={() => setAboutPage(true)}>Let's Play!</button>
